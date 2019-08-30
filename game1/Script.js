8709ace6a3873f5f0e79d40734485547ab083dd6
Neto-web <54342538+Neto-web@users.noreply.github.com>
2019-08-30 16:19:23 +0900

    Update Script.js

--- game1/Script.js
+++ game1/Script.js
@@ -4,7 +4,7 @@ window.onload = function () {
 	var game = new Game(400, 500);  				//画面サイズを400*500にする。（このサイズだとスマホでも快適なのでおススメ）
 
 	//結果ツイート時にURLを貼るため、このゲームのURLをここに記入
-	var url = "https://twitter.com/hothukurou";
+	var url = "https://neto-web.github.io/game1/index.html";
 	url = encodeURI(url); //きちんとURLがツイート画面に反映されるようにエンコードする
 	/////////////////////////////////////////////////
 	//ゲーム開始前に必要な画像・音を読み込む部分
@@ -172,7 +172,7 @@ window.onload = function () {
 		S_Tweet.ontouchend = function () {				//S_Tweetボタンをタッチした（タッチして離した）時にこの中の内容を実行する
 			//ツイートＡＰＩに送信
 
-			window.open("http://twitter.com/intent/tweet?text=何とか" + Point + "体捕獲した&hashtags=ahoge&url=" + url); //ハッシュタグにahogeタグ付くようにした。
+			window.open("http://twitter.com/intent/tweet?text=何とか" + Point + "体捕獲した" + url); //ハッシュタグにahogeタグ付くようにした。
 		};
 
 	};
